ab3349c5baf739893ec1c91acc6a045bcec4014a
tennsu <donghuagong731@gmail.com>
2025-09-27 16:07:06 +0900

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,2 +1,2 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-export const supabase = createClient('https://YOUR-PROJECT.supabase.co', 'PUBLIC-ANON-KEY');
+export const supabase = createClient('https://ksxivskpgxsvfvhbrhhh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzeGl2c2twZ3hzdmZ2aGJyaGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NTI3MDgsImV4cCI6MjA3NDUyODcwOH0.NY48ixkZ-GVt9CO-TeyZmkVG4kyEgE5V3YC3iId-lF0');
